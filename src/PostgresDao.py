972afd646e42c1311e6f43d828cb03c2b67e6bd5
clakinger <clakinger@gmail.com>
2022-10-21 21:22:49 -0500

    fix bug with user_disabled

--- src/PostgresDao.py
+++ src/PostgresDao.py
@@ -148,9 +148,4 @@ class PostgresDao:
     def is_user_disabled(self, username):
         self.cursor.execute("SELECT user_disabled FROM user_info where username = \'{0}\'".format(username))
         rows = self.cursor.fetchall()
-        print(username)
-        print(rows[0][0])
-        if rows[0][0] == 'true':
-            return True
-        else:
-            return False
+        return rows[0][0]
